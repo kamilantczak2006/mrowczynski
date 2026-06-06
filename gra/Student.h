a171e2de95a265f39257a9581d781474c09a9683
kamilantczak2006 <kamilantczak2006@gmail.com>
2026-06-06 16:59:33 +0200

    Refactor Student class header for clarity

--- gra/Student.h
+++ gra/Student.h
@@ -1,27 +1,25 @@
 #pragma once
 #include "GameObject.h"
 
-// Dziedziczenie: Student "jest" GameObjectem
+
 class Student : public GameObject {
-    // Sekcja PRIVATE: Zmienne specyficzne tylko i wyłącznie dla Studenta.
-    // Nawet inne obiekty w grze nie mogą ich bezpośrednio modyfikować.
+   
 private:
     int health;
     int maxHealth;
 
-    // Zmienne do obsługi dasha
+ 
     bool isDashing;
     float dashTimer;
     float dashCooldown;
     float dashMultiplier;
 
-    // Sekcja PUBLIC: Interfejs Studenta
+   
 public:
-    // Konstruktor Studenta
+
     Student(float startX, float startY);
 
-    // Nadpisujemy (override) wirtualne metody z klasy bazowej
-    // Parametr 'dt' (Delta Time) zapewni ruch niezależny od FPS
+   
     void update(float dt) override;
     void draw(sf::RenderWindow& window) override;
 
