075ad8771794d926bb62e8c7cc5d6b349cea80b1
kamilantczak2006 <kamilantczak2006@gmail.com>
2026-06-06 16:57:29 +0200

    Refactor GameObject class structure and comments

--- gra/GameObject.h
+++ gra/GameObject.h
@@ -2,29 +2,26 @@
 #include <SFML/Graphics.hpp>
 
 class GameObject {
-    // Sekcja PROTECTED: Zmienne tutaj są ukryte przed resztą programu,
-    // ale klasy dziedziczące (np. Student) będą miały do nich bezpośredni dostęp.
+  
 protected:
-    sf::RectangleShape shape; // Wygląd obiektu
-    sf::Vector2f position;    // Pozycja X, Y
-    float speed;              // Prędkość w pikselach na sekundę (!)
-    float rotationSpeed;      // Prędkość obrotu w stopniach na sekundę (!)
+    sf::RectangleShape shape; 
+    sf::Vector2f position;    
+    float speed;              
+    float rotationSpeed;      
 
-    // Sekcja PUBLIC: Metody dostępne z każdego miejsca w kodzie.
+    
 public:
-    // Konstruktor
+   
     GameObject(float startX, float startY, float startSpeed);
 
-    // Wirtualny destruktor - niezbędny, by uniknąć wycieków pamięci przy polimorfizmie!
+  
     virtual ~GameObject() = default;
 
-    // --- POLIMORFIZM ---
-    // Czysto wirtualne metody (oznaczone jako '= 0').
-    // To zmusza każdą klasę pochodną do napisania własnej wersji tych funkcji.
+
     virtual void update(float dt) = 0;
     virtual void draw(sf::RenderWindow& window) = 0;
 
-    // Zwykłe gettery, żeby sprawdzić, gdzie jest obiekt i czy z czymś koliduje
+    
     sf::Vector2f getPosition() const;
     sf::FloatRect getBounds() const;
 };
