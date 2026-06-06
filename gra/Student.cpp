46bb3194b6800ff7135a4901fa2f74f8ab2431f9
kamilantczak2006 <kamilantczak2006@gmail.com>
2026-06-06 16:58:54 +0200

    Refactor Student class constructor and update method

--- gra/Student.cpp
+++ gra/Student.cpp
@@ -1,8 +1,8 @@
 #include "Student.h"
 
-// Konstruktor Studenta - najpierw wywołuje konstruktor GameObject
+
 Student::Student(float startX, float startY)
-    : GameObject(startX, startY, 300.0f) // 300.0f to nasza bazowa prędkość (piksele na sekundę)
+    : GameObject(startX, startY, 300.0f)
 {
     health = 100;
     maxHealth = 100;
@@ -11,14 +11,14 @@ Student::Student(float startX, float startY)
     dashCooldown = 0.0f;
     dashMultiplier = 4.0f;
 
-    // Konfigurujemy wygląd naszego prostokąta
+  
     shape.setSize(sf::Vector2f(50.0f, 50.0f));
     shape.setFillColor(sf::Color::Green);
     shape.setPosition(position);
 }
 
 void Student::update(float dt) {
-    // 1. Obsługa odliczania czasu dasha (w sekundach!)
+    
     if (dashCooldown > 0.0f) {
         dashCooldown -= dt;
     }
@@ -29,27 +29,27 @@ void Student::update(float dt) {
         }
     }
 
-    // 2. Aktywacja dasha (Spacja)
+   
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && dashCooldown <= 0.0f) {
         isDashing = true;
         dashTimer = 0.2f;       // Dash trwa 0.2 sekundy
         dashCooldown = 1.0f;    // Musimy poczekać 1 sekundę na kolejny
     }
 
-    // 3. Obliczanie prędkości
+    
     float currentSpeed = speed;
     if (isDashing) {
         currentSpeed *= dashMultiplier;
     }
 
-    // 4. Sterowanie (WASD)
+    // Sterowanie (WASD)
     sf::Vector2f movement(0.0f, 0.0f);
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) movement.y -= 1.0f;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) movement.y += 1.0f;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) movement.x -= 1.0f;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) movement.x += 1.0f;
 
-    // 5. Aktualizacja pozycji: Dodajemy wektor kierunku przemnożony przez prędkość i czas klatki
+    //Aktualizacja pozycji
     position += movement * currentSpeed * dt;
 
     // 6. Ściany
@@ -58,7 +58,7 @@ void Student::update(float dt) {
     if (position.y < 0) position.y = 0;
     if (position.y > 720 - 50) position.y = 720 - 50;
 
-    // 7. Zapisanie nowej pozycji do obiektu rysującego
+    // Zapisanie nowej pozycji
     shape.setPosition(position);
 }
 
